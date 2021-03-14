22e9135a6d9553a650d146f3576965f2d5abe89d
Khondokar Amir Hossain <amirkhondokar@gmail.com>
2021-03-14 16:51:00 +0600

    new_developed

---
+++ 25_Cryptographic_Algos/distributed_mqtt_agent.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 14 16:25:58 2021
+
+@author: amiriot
+@file: distributed_mqtt_agent.py
+@purpose: to build a distributed network of communication over MQTT protocol
+"""
+import paho.mqtt
+
+class MqttAgent(paho.mqtt):
+    
+    def __init__(self):
+        pass
+    
+    def handShakeWith(self,address):
+        pass
+    
+    def sendMessageToHost(self,message):
+        pass
+    
+    def receiveMessageFromHost(self,message):
+        pass
+    
+
--- 25_Cryptographic_Algos/public_key.py
+++ 25_Cryptographic_Algos/public_key.py
@@ -1,13 +1,26 @@
+import time
 import rsa
 
-data = "rango_isbackagain"
-
-pub,priv = rsa.newkeys(2048)
-print(pub)
-
-print(priv)
-
-enc = rsa.encrypt(data.encode('utf-16'),pub)
-print(enc)
-print(rsa.decrypt(enc,priv).decode('utf-16'))
+data = "rangoisbackagainr"
+for i in range(9,10):
+    keysize = 2**i
+    st = time.time()
+    print("Key Size: ",keysize)
+    pub,priv = rsa.newkeys(keysize)
+    
+    et = time.time()
+    print("Time taken : ",et-st,"Seconds")
+    
+    #print(type(priv))
+    #print(len(priv))
+    pa = rsa.key.PublicKey(priv.n,priv.e)
+    k = str(priv)[11:-1]
+    #print(k)
+    p = k.rstrip().split(",")
+    print("Comp : ",len(p))
+    #print(p)
+    
+    enc = rsa.encrypt(data.encode('utf-16'),pa)
+    #print(enc)
+    print(rsa.decrypt(enc,rsa.key.PrivateKey(int(p[0]),int(p[1]),int(p[2]),int(p[3]),int(p[4]))).decode('utf-16'))
 
